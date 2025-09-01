1005e4778912fb1f8628dd23bd928934d94c24fe
Helena <helenacarrico12@gmail.com>
2025-09-01 19:41:37 -0300

    Repetir o jogo

--- projeto c++ jogo da forca/main.cpp
+++ projeto c++ jogo da forca/main.cpp
@@ -30,78 +30,72 @@ public:
     JogoForca() {
         srand((unsigned)time(NULL));
 
-        
+        // Arte ASCII
         forcaAscii.push_back(
-			"   _______\n"
-			"  |/      |\n"
-			"  |       \n"
-			"  |       \n"
-			"  |       \n"
-			"  |       \n"
-			" _|___    \n"
-		);
-			
-		forcaAscii.push_back(
-			"   _______\n"
-			"  |/      |\n"
-			"  |      (_)\n"
-			"  |       \n"
-			"  |       \n"
-			"  |       \n"
-			" _|___    \n"
-		);
-			
-		forcaAscii.push_back(
-			"   _______\n"
-			"  |/      |\n"
-			"  |      (_)\n"
-			"  |       |\n"
-			"  |       \n"
-			"  |       \n"
-			" _|___    \n"
-		);
-			
-		forcaAscii.push_back(
-			"   _______\n"
-			"  |/      |\n"
-			"  |      (_)\n"
-			"  |      /|\n"
-			"  |       \n"
-			"  |       \n"
-			" _|___    \n"
-		);
-			
-		forcaAscii.push_back(
-			"   _______\n"
-			"  |/      |\n"
-			"  |      (_)\n"
-			"  |      /|\\\n"
-			"  |       \n"
-			"  |       \n"
-			" _|___    \n"
-		);
-			
-		forcaAscii.push_back(
-			"   _______\n"
-			"  |/      |\n"
-			"  |      (_)\n"
-			"  |      /|\\\n"
-			"  |      / \n"
-			"  |       \n"
-			" _|___    \n"
-		);
-			
-		forcaAscii.push_back(
-			"   _______\n"
-			"  |/      |\n"
-			"  |      (_)\n"
-			"  |      /|\\\n"
-			"  |      / \\\n"
-			"  |       \n"
-			" _|___    \n"
-		);
+            "   _______\n"
+            "  |/      |\n"
+            "  |       \n"
+            "  |       \n"
+            "  |       \n"
+            "  |       \n"
+            " _|___    \n"
+        );
+        forcaAscii.push_back(
+            "   _______\n"
+            "  |/      |\n"
+            "  |      (_)\n"
+            "  |       \n"
+            "  |       \n"
+            "  |       \n"
+            " _|___    \n"
+        );
+        forcaAscii.push_back(
+            "   _______\n"
+            "  |/      |\n"
+            "  |      (_)\n"
+            "  |       |\n"
+            "  |       \n"
+            "  |       \n"
+            " _|___    \n"
+        );
+        forcaAscii.push_back(
+            "   _______\n"
+            "  |/      |\n"
+            "  |      (_)\n"
+            "  |      /|\n"
+            "  |       \n"
+            "  |       \n"
+            " _|___    \n"
+        );
+        forcaAscii.push_back(
+            "   _______\n"
+            "  |/      |\n"
+            "  |      (_)\n"
+            "  |      /|\\\n"
+            "  |       \n"
+            "  |       \n"
+            " _|___    \n"
+        );
+        forcaAscii.push_back(
+            "   _______\n"
+            "  |/      |\n"
+            "  |      (_)\n"
+            "  |      /|\\\n"
+            "  |      / \n"
+            "  |       \n"
+            " _|___    \n"
+        );
+        forcaAscii.push_back(
+            "   _______\n"
+            "  |/      |\n"
+            "  |      (_)\n"
+            "  |      /|\\\n"
+            "  |      / \\\n"
+            "  |       \n"
+            " _|___    \n"
+        );
 
-
+        // Palavras
         palavras.push_back("bolsonaro"); palavras.push_back("elizabeth");
         palavras.push_back("stalin");    palavras.push_back("socrates");
         palavras.push_back("hitler");   palavras.push_back("ronildo");
@@ -141,7 +135,6 @@ public:
 
             if (letrasUsadas.count(letra)) {
                 cout << "Você ja tentou essa letra!\n";
-                
                 int p = 0; while (p < 1) ++p;
                 continue;
             }
@@ -160,7 +153,6 @@ public:
             if (!achou) erros++;
         }
 
-        
         if (palavraOculta == palavra) {
             cout << "\nVoce ganhou, parabens! A palavra correta era: " << palavra << "\n";
         } else {
@@ -171,8 +163,14 @@ public:
 };
 
 int main() {
-    JogoForca jogo;
-    jogo.jogar();
+    char r = 's';
+    while (r == 's' || r == 'S') {
+        JogoForca jogo;
+        jogo.jogar();
+
+        cout << "Deseja jogar novamente? (s/n): ";
+        cin >> r;
+    }
     return 0;
 }
 
