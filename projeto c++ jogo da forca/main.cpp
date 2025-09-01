d6f1a3c7c7d8150592ed27a0e2c0c3d64a6f2d23
Alex Santos <alexsantosapolinario@gmail.com>
2025-09-01 19:17:39 -0300

    melhor interface, melhor arte ascii, temas colocados baseado no bloco de notas

--- projeto c++ jogo da forca/main.cpp
+++ projeto c++ jogo da forca/main.cpp
@@ -31,24 +31,87 @@ public:
         srand((unsigned)time(NULL));
 
         
-        forcaAscii.push_back("  +---+\n  |   |\n      |\n      |\n      |\n      |\n=======");
-        forcaAscii.push_back("  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=======");
-        forcaAscii.push_back("  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=======");
-        forcaAscii.push_back("  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=======");
-        forcaAscii.push_back("  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=======");
-        forcaAscii.push_back("  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=======");
-        forcaAscii.push_back("  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=======");
+        forcaAscii.push_back(
+			"   _______\n"
+			"  |/      |\n"
+			"  |       \n"
+			"  |       \n"
+			"  |       \n"
+			"  |       \n"
+			" _|___    \n"
+		);
+			
+		forcaAscii.push_back(
+			"   _______\n"
+			"  |/      |\n"
+			"  |      (_)\n"
+			"  |       \n"
+			"  |       \n"
+			"  |       \n"
+			" _|___    \n"
+		);
+			
+		forcaAscii.push_back(
+			"   _______\n"
+			"  |/      |\n"
+			"  |      (_)\n"
+			"  |       |\n"
+			"  |       \n"
+			"  |       \n"
+			" _|___    \n"
+		);
+			
+		forcaAscii.push_back(
+			"   _______\n"
+			"  |/      |\n"
+			"  |      (_)\n"
+			"  |      /|\n"
+			"  |       \n"
+			"  |       \n"
+			" _|___    \n"
+		);
+			
+		forcaAscii.push_back(
+			"   _______\n"
+			"  |/      |\n"
+			"  |      (_)\n"
+			"  |      /|\\\n"
+			"  |       \n"
+			"  |       \n"
+			" _|___    \n"
+		);
+			
+		forcaAscii.push_back(
+			"   _______\n"
+			"  |/      |\n"
+			"  |      (_)\n"
+			"  |      /|\\\n"
+			"  |      / \n"
+			"  |       \n"
+			" _|___    \n"
+		);
+			
+		forcaAscii.push_back(
+			"   _______\n"
+			"  |/      |\n"
+			"  |      (_)\n"
+			"  |      /|\\\n"
+			"  |      / \\\n"
+			"  |       \n"
+			" _|___    \n"
+		);
 
-        palavras.push_back("cachorro"); palavras.push_back("banana");
-        palavras.push_back("brasil");    palavras.push_back("carro");
-        palavras.push_back("futebol");   palavras.push_back("azul");
-        palavras.push_back("mesa");      palavras.push_back("medico");
-        palavras.push_back("amor");      palavras.push_back("ouro");
-        palavras.push_back("batman");    palavras.push_back("matrix");
-        palavras.push_back("naruto");    palavras.push_back("computador");
-        palavras.push_back("pizza");     palavras.push_back("piano");
-        palavras.push_back("praia");     palavras.push_back("lua");
-        palavras.push_back("fogo");      palavras.push_back("coruja");
+
+        palavras.push_back("bolsonaro"); palavras.push_back("elizabeth");
+        palavras.push_back("stalin");    palavras.push_back("socrates");
+        palavras.push_back("hitler");   palavras.push_back("ronildo");
+        palavras.push_back("napoleao");      palavras.push_back("jesus");
+        palavras.push_back("trotsky");      palavras.push_back("salomao");
+        palavras.push_back("gengis");    palavras.push_back("diogenes");
+        palavras.push_back("cleopatra");    palavras.push_back("tchaikovsky");
+        palavras.push_back("antonieta");     palavras.push_back("trump");
+        palavras.push_back("darc");     palavras.push_back("francisco");
+        palavras.push_back("galileu");      palavras.push_back("descartes");
 
         palavra = palavras[rand() % palavras.size()];
         palavraOculta = string(palavra.size(), '_');
@@ -59,11 +122,11 @@ public:
         while (erros < (int)forcaAscii.size() - 1 && palavraOculta != palavra) {
             limparTela();
 
-            cout << "=== JOGO DA FORCA ===\n\n";
+            cout << "Bem vindo(a) ao jogo da forca sobre figuras historicas!\n\n";
             cout << forcaAscii[erros] << "\n";
             cout << "Palavra: " << palavraOculta << "\n";
 
-            cout << "Letras usadas: ";
+            cout << "Letras ja usadas: ";
             set<char>::iterator it = letrasUsadas.begin();
             while (it != letrasUsadas.end()) {
                 cout << *it << ' ';
@@ -77,7 +140,7 @@ public:
             letra = (char)tolower((unsigned char)letra);
 
             if (letrasUsadas.count(letra)) {
-                cout << "Você já tentou essa letra!\n";
+                cout << "Você ja tentou essa letra!\n";
                 
                 int p = 0; while (p < 1) ++p;
                 continue;
@@ -99,10 +162,10 @@ public:
 
         
         if (palavraOculta == palavra) {
-            cout << "\nParabéns! Você acertou: " << palavra << "\n";
+            cout << "\nVoce ganhou, parabens! A palavra correta era: " << palavra << "\n";
         } else {
             cout << forcaAscii.back() << "\n";
-            cout << "\nGame Over! A palavra era: " << palavra << "\n";
+            cout << "\nVoce perdeu! A palavra era: " << palavra << "\n";
         }
     }
 };
